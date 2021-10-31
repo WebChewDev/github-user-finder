f962da37b4ab10f49b33f6ef9997c4bb330e9e97
Daniel <Webchews@gmail.com>
2021-10-31 10:17:24 +0000

    initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {
+    msg: String,
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
